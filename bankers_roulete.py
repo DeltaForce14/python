69f4d4fe42abed47487adcddab249b56a833dc19
Dita Pesek <33196424+DeltaForce14@users.noreply.github.com>
2022-05-24 23:23:00 +0100

    Update bankers_roulete.py

--- bankers_roulete.py
+++ bankers_roulete.py
@@ -1,17 +1,25 @@
 
-# Select a random name from a list of names. The person selected will have to pay 
-# for everybody's food bill.
+#Choose random name to pay the bill
 
 import random
 
-names = input("Input names separated by coma: ")
-names_list = names.split(",")
-lenght_list = len(names_list)
+#Input: list of names
+names_string = input("Give me everybody's names, separated by a comma. ")
 
-random_choice = (random.randint(0, lenght_list-1))
+#Split names into the list 
+names = names_string.split(", ")
 
-chosen_person = names_list[random_choice]
 
-print(chosen_person)
+# List with index number of items form list 'names'
+index_list = []
+for n in range(0, len(names)):
+    index_list.append(n)
+
+# Choose random number from the index list 
+random_number = random.choice(index_list)
+   
+#Print name using random choice 
+print(names[random_number])
+
 
 
